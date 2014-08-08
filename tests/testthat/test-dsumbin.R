82e67c7e4b93932bc03122a64b75ed988be014e9
Jerome V. Braun <jerome.braun@kmri.com>
2014-08-08 10:28:04 -0700

    Added test for sum of one binomial

--- tests/testthat/test-dsumbin.R
+++ tests/testthat/test-dsumbin.R
@@ -4,3 +4,6 @@ test_that("adding two binomials works", {
   expect_that(dsumbin(10, size=c(5,5), prob=c(1, 1)), equals(1))
 })
 
+test_that("one binomial works", {
+  expect_that(dsumbin(3, size=c(5), prob=(.3)), not(throws_error())) 
+})
